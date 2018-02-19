2392fc7f445836a2705ce7cb6d795620345a1181
Carlos <charliedn@Gmail.com>
2018-02-19 16:36:04 +0000

    First commit

---
+++ app/javascript/app.vue
@@ -0,0 +1,24 @@
+<template>
+  <div id="app">
+    <p>{{ message }}</p>
+    <p>{{ messaga }}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  data: function () {
+    return {
+      message: "Hello Vuasljfkasklje!",
+      messaga: "fart"
+    }
+  }
+}
+</script>
+
+<style scoped>
+p {
+  font-size: 2em;
+  text-align: center;
+}
+</style>
