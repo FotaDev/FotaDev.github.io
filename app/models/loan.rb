a35ff2630575537d1455faac01877d3d3b791429
Carlos <charliedn@Gmail.com>
2018-02-25 05:30:37 +0000

    loan

--- app/models/loan.rb
+++ app/models/loan.rb
@@ -2,6 +2,13 @@
 #
 # Table name: loans
 #
+#  id         :integer          not null, primary key
+#  hire_id    :integer
+#  name_tag   :string(255)
+#  date_out   :datetime
+#  date_back  :datetime
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
 #
 
 class Loan < ApplicationRecord
