dbd513113348b64fa0ebea97072d49cb7e24c168
jacas <jacek.budzynski.92@gmail.com>
2018-02-26 17:48:13 +0000

    dunno

--- app/models/user.rb
+++ app/models/user.rb
@@ -43,19 +43,15 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable, :confirmable, :lockable
 
-
-  def active_for_authentication? 
+  def active_for_authentication?
     super && approved?
   end
-  
-  def inactive_message 
-    if !approved? 
-      :not_approved 
-    else 
-      super # Use whatever other message 
-    end 
+
+  def inactive_message
+    if !approved?
+      :not_approved
+    else
+      super # Use whatever other message
+    end
   end
-
-
-
 end
